f73a0876d274c4d96f89e4f12c29e87b83c5f82a
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-04 19:34:29 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -2,8 +2,13 @@ class Fish
   # code goes here
   attr_reader :name
 
-  def initialize (name)
+  def initialize (name, mood = "nervous")
     @name = name
+    @mood = mood
+  end
+
+  def mood= (mood)
+    @mood = mood
   end
 
 end
