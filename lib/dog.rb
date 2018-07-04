c7f1fad63850392a690b4008d739f48f00ec27c2
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-04 19:32:12 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,10 @@
 class Dog
   # code goes here
-  attr_reader :name 
+  attr_reader :name
 
-  def initialize (name)
+  def initialize (name, mood = "nervous")
     @name = name
+    @mood = mood
   end
 
 end
