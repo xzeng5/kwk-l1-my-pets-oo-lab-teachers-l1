6f9e07b7dcc624cdb78317a14be7a7f4b04d2dfc
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-04 19:36:26 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,7 +1,14 @@
 class Owner
   # code goes here
-  attr_reader :species
+  attr_reader :species, :mood
 
-  def initialize (species)
-    @name = species
+  def initialize (species, mood = "nervous")
+    @species = species
+    @mood = mood
   end
+
+  def mood= (mood)
+    @mood = mood
+  end
+
+ end
