9621318e22fc8894421023704b7decbae6d584f2
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-04 19:35:37 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,14 @@
 class Owner
   # code goes here
+  attr_reader :name, :mood
+  
+  def initialize (name, mood = "nervous")
+    @name = name 
+    @mood = mood
+  end
 
+  def mood= (mood)
+    @mood = mood
+  end
+  
  end
