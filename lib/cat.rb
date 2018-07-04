72ba298f4484f9eba513f5def545144b3f723d4c
Anna Zeng <xiange.anna.zeng@gmail.com>
2018-07-04 19:08:30 +0000

    automatically backed up by learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,17 @@
 class Cat
   # code goes here
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def mood= (mood)
+    @mood = mood
+  end
+
+  def mood
+    @mood
+  end
+
 end
